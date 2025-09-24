3f6698b2cace0d8b1f6f4e3791db1be87e1c99d9
xiej2520 <jackyxie2520@outlook.com>
2025-09-23 20:18:41 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "site";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    emscripten
+                  ]
+                  ++ (with pkgs; [
+                    (python3.withPackages (
+                      python-pkgs: with python-pkgs; [
+                      ]
+                    ))
+                  ]);
+              };
+        }
+      );
+    };
+}
